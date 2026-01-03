09ef084c2c946a34eaa8b7b47bfa84b7ae2c7f76
arcadefl <whosarcadefl@gmail.com>
2026-01-03 12:11:51 +0100

    Add files via upload

---
+++ BACK ON TRACK 2026/pwa-files/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'productivity-hub-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.svg',
+  '/icon-512.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
